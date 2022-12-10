9b53551bd4f46acc08de47d8dceec09cd1819723
Mika Väisänen <vaisasenmika89@outlook.com>
2022-12-11 00:03:48 +0200

    A console app that outputs text to the screen

---
+++ Chapter02/ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Output text to the screen.
+            Console.WriteLine("The first app in Beginning C# and .NET!");
+            Console.ReadKey();
+        }
+    }
+}
